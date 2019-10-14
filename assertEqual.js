4ecbfc824660e4b2fbe593e7c70de1114980a8c9
andyzen619 <andyzen619@gmail.com>
2019-10-14 21:52:19 +0000

    Finished assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,5 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
-  /**
-  if (actual === expected) {
-    console.log("Assertion Passed: " + "\"" + actual + "\"" + " === " + "\"" + expected + "\"");
-  } else {
-    console.log("Assertion Failed: " + "\"" + actual + "\"" + " !==" + " \"" + expected + "\"");
-  }
-  */
-
-
   if (actual === expected) {
     console.log(`Assertion Passed: "${actual}" === "${expected}"`);
   } else {
