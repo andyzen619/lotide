f82b5a6141a2d24cbc48c713f3d18e0626c50d66
andyzen619 <andyzen619@gmail.com>
2019-10-15 22:05:16 +0000

    added comments to flatten

--- flatten.js
+++ flatten.js
@@ -51,10 +51,13 @@ const assertArraysEqual = function(arr1, arr2) {
  */
 const flatten = function(array) {
   let result = [];
+
+  //Iterate through array items push if not array
   for (i in array) {
     if (!Array.isArray(array[i])) {
       result.push(array[i]);
     } else {
+      //If is an array, iterate through nested array and push to result.
       for (j in array[i]) {
         result.push(array[i][j]);
       }
