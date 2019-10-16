d18e8976bbe299dd04d03c1047cef59b261e3a95
andyzen619 <andyzen619@gmail.com>
2019-10-16 16:26:40 +0000

    Added findKeyByValue functionality

---
+++ findKeyByValue.js
@@ -0,0 +1,84 @@
+/**
+ * Returns true if actual is equal to expected, false if otherwise.
+ * @param {*} actual 
+ * @param {*} expected 
+ */
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`Assertion Failed: "${actual}" !== "${expected}"`);
+  };
+}
+
+/**
+ * Returns true if arr1 and arr2 are equal and false otherwise.
+ * @param {*} arr1 array to be compared.
+ * @param {*} arr2 array to be compared.
+ */
+let eqArrays = function(arr1, arr2) {
+  //Checks for equal length, return false if otherwise.
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    //Loops through items or array and checks if they are equal, return false if otherwise.
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * Returns assertion statment for comparing arrays arr1 and arr2.
+ * @param {*} arr1 
+ * @param {*} arr2 
+ */
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`Assertion PASSED: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`Assertion FAILED: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+/**
+ * Returns a new array that contains items in arr1 that are not in arr2.
+ * @param {*} arr1 
+ * @param {*} arr2 
+ */
+const without = function(arr1, arr2) {
+  let result = [];
+
+  //Loop through items in arr1 
+  for (i in arr1) {
+    if (!arr2.includes(arr1[i])) {
+      result.push(arr1[i]);
+    }
+  }
+  return result;
+}
+
+/**
+ * Return the key in which the first occurence of val appears in obj.
+ * @param {Object} obj 
+ * @param {Object} val 
+ */
+const findKeyByValue = function(obj, val) {
+  for (key in obj) {
+    if (obj[key] == val) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
