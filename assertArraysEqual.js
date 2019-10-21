3b6b7a6ce2a28b2ee3769425ce26711b2e3552b1
andyzen619 <andyzen619@gmail.com>
2019-10-21 15:33:34 +0000

    Added assertArraysEquals test and refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,4 @@
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
-
-/**
- * Returns true if arr1 and arr2 are equal and false otherwise.
- * @param {*} arr1 array to be compared.
- * @param {*} arr2 array to be compared.
- */
-let eqArrays = function(arr1, arr2) {
-  //Checks for equal length, return false if otherwise.
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    //Loops through items or array and checks if they are equal, return false if otherwise.
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
+const eqArrays = require("./eqArrays");
 
 /**
  * Returns assertion statment for comparing arrays arr1 and arr2.
@@ -34,6 +13,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([], []);
-assertArraysEqual([1, 1, 1, 1], [1, 1]);
-assertArraysEqual([12344], []);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualsTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([], []);
+assertArraysEqual([1, 1, 1, 1], [1, 1]);
+assertArraysEqual([12344], []);
